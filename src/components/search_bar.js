e66a87ad04ebecd363cc1075b6e471d79c4545ea
angel.martin.local <angel.martin@babel.es>
2022-03-16 20:46:40 +0100

    OnChange input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+
+    constructor(props) { 
+        super(props);
+        this.state = { term: '' };
+    }
+
     render() { 
         return (
             <div className="search-bar">
-                <input placeholder="Search for videos" />
+                <input
+                    onChange={(e) => this.setState({ term: e.target.value })} value={this.state.term} placeholder="Search for videos" />
+                Value of the input: { this.state.term }
             </div>
         );
     }
