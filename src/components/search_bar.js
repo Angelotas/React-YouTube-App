b29797f813ade223a518b9d10f07238e979dc72b
angel.martin.local <angel.martin@babel.es>
2022-03-16 21:15:13 +0100

    Componente App como clase y creando instancia de YouTube.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,16 @@ class SearchBar extends Component {
 
     constructor(props) { 
         super(props);
-        this.state = { term: '' };
+        this.state = { term: 'Starting value' };
     }
 
     render() { 
         return (
             <div className="search-bar">
                 <input
-                    onChange={(e) => this.setState({ term: e.target.value })} value={this.state.term} placeholder="Search for videos" />
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)}
+                    placeholder="Search for videos" />
                 Value of the input: { this.state.term }
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-const API_KEY = '******************************************';
+const API_KEY = '*****';
 
 // Create a new component. This component should produce some HTML
-const App = () =>
-    <div>
-        <h1>Hi!</h1>
-        <SearchBar />
-    </div>;
+class App extends Component {
+
+    constructor(props) { 
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'sufboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return <div>
+            <h1>Hi!</h1>
+            <SearchBar />
+        </div>;
+    }
+} 
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
